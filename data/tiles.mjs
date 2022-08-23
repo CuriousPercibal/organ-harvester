438427aa1fde4d07dfc1540bc507d02eec0bddaf
Imre Molnar <wkzina@inf.elte.hu>
2022-08-23 11:24:43 +0200

    Add rotated belts

--- data/tiles.mjs
+++ data/tiles.mjs
@@ -12,6 +12,21 @@ export const tileSet = [
   {
     "id": 1,
     "name": "belt",
-    "src": "assets/buildings/belt.svg"
+    "src": "assets/buildings/belt_n.svg"
+  },
+  {
+    "id": 2,
+    "name": "belt",
+    "src": "assets/buildings/belt_e.svg"
+  },
+  {
+    "id": 3,
+    "name": "belt",
+    "src": "assets/buildings/belt_s.svg"
+  },
+  {
+    "id": 4,
+    "name": "belt",
+    "src": "assets/buildings/belt_w.svg"
   }
 ]
