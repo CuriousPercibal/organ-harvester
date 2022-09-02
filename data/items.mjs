341a5f030a1e5afd84f0bad07dba4dc7b39c14fc
Imre Molnar <wkzina@inf.elte.hu>
2022-09-02 11:50:45 +0200

    Add live, and rotten liver

--- data/items.mjs
+++ data/items.mjs
@@ -26,12 +26,47 @@ export const itemSet = [
     },
     {
         "id": ITEMS.CASKET,
-        "name": "coffin",
+        "name": "casket",
         "src": "assets/items/casket.svg"
     },
     {
         "id": ITEMS.URN,
-        "name": "coffin",
+        "name": "urn",
+        "src": "assets/items/urn.svg"
+    },
+    {
+        "id": ITEMS.LIVER,
+        "name": "liver",
+        "src": "assets/items/liver.svg"
+    },
+    {
+        "id": ITEMS.BAD_LIVER,
+        "name": "rotten liver",
+        "src": "assets/items/bad_liver.svg"
+    },
+    {
+        "id": ITEMS.HEART,
+        "name": "heart",
+        "src": "assets/items/urn.svg"
+    },
+    {
+        "id": ITEMS.BAD_HEART,
+        "name": "rotten heart",
+        "src": "assets/items/urn.svg"
+    },
+    {
+        "id": ITEMS.KIDNEY,
+        "name": "kidney",
+        "src": "assets/items/urn.svg"
+    },
+    {
+        "id": ITEMS.BAD_KIDNEY,
+        "name": "rotten kidney",
+        "src": "assets/items/urn.svg"
+    },
+    {
+        "id": ITEMS.PATIENT,
+        "name": "patient",
         "src": "assets/items/urn.svg"
     }
 ]
